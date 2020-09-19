66673e956b8bda1997145ef6e9e0f69544ae2dfe
ddtt786 <69110424+ddtt786@users.noreply.github.com>
2020-09-19 15:52:11 +0900

    Update and rename code.js to import.js

---
+++ import.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('blockquote').forEach(el => {
+    var ines = el.innerHTML;
+    el.innerHTML = "<img src='따옴표.png' style=\"width:30px;margin-right:10px\">"+ines+"<img src='따옴표2.png' style=\"width:30px;margin-left:10px\">";
+})
+
+document.querySelectorAll('zacpum').forEach(el => {
+    var zac = el.innerHTML;
+    el.innerHTML = "<iframe src='https://playentry.org/api/iframe/project/"+zac+"' style='width: 600px\;height: 360px\;border-radius: 8px\;border: 0\;margin:10px'>";
+})
+
+document.querySelectorAll('ttl').forEach(el => {
+    var zac = el.innerHTML;
+    el.innerHTML = "<div class='ttl'>"+zac+"</div>";
+})
+
+document.querySelectorAll('stmp').forEach(el => {
+    var zac = el.innerHTML;
+    el.innerHTML = "<div class='stmp'>"+zac+"</div>";
+})
+
+document.querySelectorAll('end').forEach(el => {
+    var zac = el.innerHTML;
+    el.innerHTML = "<div class='end'>"+zac+"</div>";
+})
+
